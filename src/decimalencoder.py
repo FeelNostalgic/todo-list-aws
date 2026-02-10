c18d69bd89b151faf27a90471ee8367fa12bd989
Francisco José Aragonés de la Rosa <franciscoaragones2014@gmail.com>
2026-02-10 18:18:45 +0100

    Forzando deploy para reto 5

--- src/decimalencoder.py
+++ src/decimalencoder.py
@@ -7,4 +7,5 @@ class DecimalEncoder(json.JSONEncoder):
     def default(self, obj):
         if isinstance(obj, decimal.Decimal):
             return int(obj) 
+            
         return super(DecimalEncoder, self).default(obj)
